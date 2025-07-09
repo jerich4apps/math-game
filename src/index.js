42bfe8601467c4f546a34d6c4f9475008823d1ce
jerich4apps <richmond.jay@gmail.com>
2025-07-08 21:10:21 -0500

    Initial commit for math game

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MathGameApp from './MathGameApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MathGameApp />
+  </React.StrictMode>
+);
